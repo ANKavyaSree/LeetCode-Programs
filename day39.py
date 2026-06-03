346462698ba5293e0f6e0dc7c587a6f540cd1945
ANKavyaSree <ankavyasri11@gmail.com>
2026-06-03 17:46:27 +0530

    Add files via upload

---
+++ day39.py
@@ -0,0 +1,67 @@
+def earliest_finish_time(land_start, land_duration, water_start, water_duration):
+    min_time = float('inf')
+
+    # Land Ride -> Water Ride
+    for i in range(len(land_start)):
+        land_finish = land_start[i] + land_duration[i]
+
+        for j in range(len(water_start)):
+            water_begin = max(land_finish, water_start[j])
+            finish_time = water_begin + water_duration[j]
+
+            min_time = min(min_time, finish_time)
+
+    # Water Ride -> Land Ride
+    for j in range(len(water_start)):
+        water_finish = water_start[j] + water_duration[j]
+
+        for i in range(len(land_start)):
+            land_begin = max(water_finish, land_start[i])
+            finish_time = land_begin + land_duration[i]
+
+            min_time = min(min_time, finish_time)
+
+    return min_time
+
+
+# User Input
+n = int(input("Enter number of land rides: "))
+
+print("Enter land ride start times:")
+land_start = list(map(int, input().split()))
+
+print("Enter land ride durations:")
+land_duration = list(map(int, input().split()))
+
+m = int(input("Enter number of water rides: "))
+
+print("Enter water ride start times:")
+water_start = list(map(int, input().split()))
+
+print("Enter water ride durations:")
+water_duration = list(map(int, input().split()))
+
+result = earliest_finish_time(
+    land_start,
+    land_duration,
+    water_start,
+    water_duration
+)
+
+print("Earliest Finish Time:", result)
+
+
+# sample input
+# Enter number of land rides: 2
+# Enter land ride start times:
+# 2 8
+# Enter land ride durations:
+# 4 1
+# Enter number of water rides: 1
+# Enter water ride start times:
+# 6
+# Enter water ride durations:
+# 3
+
+# sample output
+# Earliest Finish Time: 9
