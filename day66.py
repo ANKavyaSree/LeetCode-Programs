9ec4fda23a0cda293cad72e89f0dbe60dc49d1d7
ANKavyaSree <ankavyasri11@gmail.com>
2026-06-30 17:29:55 +0530

    Add files via upload

---
+++ day66.py
@@ -0,0 +1,24 @@
+s = input("Enter the string: ")
+
+count = {'a': 0, 'b': 0, 'c': 0}
+
+left = 0
+ans = 0
+
+for right in range(len(s)):
+    count[s[right]] += 1
+
+    while count['a'] > 0 and count['b'] > 0 and count['c'] > 0:
+        ans += len(s) - right
+        count[s[left]] -= 1
+        left += 1
+
+print("Number of valid substrings:", ans)
+
+
+
+# sample input
+# Enter the string: abcabc
+
+# sample output
+# Number of valid substrings: 10
