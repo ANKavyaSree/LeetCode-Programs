9795ce5ad010111de030efcb008245a79991ad29
ANKavyaSree <ankavyasri11@gmail.com>
2026-08-10 08:26:39 +0530

    Add files via upload

---
+++ day107.py
@@ -0,0 +1,16 @@
+# LeetCode 1510 - Stone Game IV
+# User input version
+
+n = int(input("Enter n: "))
+
+dp = [False] * (n + 1)
+
+for i in range(1, n + 1):
+    j = 1
+    while j * j <= i:
+        if not dp[i - j * j]:
+            dp[i] = True
+            break
+        j += 1
+
+print("Alice wins:", dp[n])
